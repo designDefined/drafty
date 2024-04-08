d2f5861b6a85f1fd084742a58461368e3b97207b
designDefined <designdefined.by@gmail.com>
2024-04-08 20:56:32 +0900

    feat: Delete deprecated

--- project/dialog/deprecated/react-old/index.ts
+++
@@ -1,9 +0,0 @@
-import { createIntentPolicyFactory, createViewPolicyFactory } from "./core";
-import { useView, useStaticView, useIntent } from "./hooks";
-
-const PVI = {
-  createViewPolicyFactory,
-  createIntentPolicyFactory,
-};
-
-export { PVI, useView, useStaticView, useIntent };
